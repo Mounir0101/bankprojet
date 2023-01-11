d0de07f690a6ee70cee0286baa613f8b0092c56b
nom1 <nom1@gmail.com>
2023-01-11 09:53:01 +0100

    partage du menu.php

--- src/template/partials/head.php
+++ src/template/partials/head.php
@@ -4,7 +4,10 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title><?= $page_title;?></title>
+        <title><?= $Ma_Coding_Banque;?></title>
     </head>
     <body>
+        <header>
+            
+        </header>
         
