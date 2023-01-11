7c42d03b658b0336074daa8cb170f51a367bf7da
nom1 <nom1@gmail.com>
2023-01-11 11:44:23 +0100

    mise a jour page de retrait utilisateur

--- src/init.php
+++ src/init.php
@@ -12,7 +12,7 @@ require_once __DIR__ . '/db.php';
 require_once __DIR__ . '/utils/errors.php';
 
 // pages existantes sur notre site internet
-$pages = ['home', 'contact', 'signup', 'login', 'admin_contact'];
+$pages = ['home', 'contact', 'signup', 'login', 'admin_contact', 'retrait_bdd'];
 
 // init variables vides pour le template
 $page_scripts = "";
